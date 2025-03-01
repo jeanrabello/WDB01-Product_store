6f4af69703992283a3d834ca7e52d0a158827f59
Jean Rabello <jean.rabello.prog@gmail.com>
2025-03-01 09:51:53 -0300

    refactor: Add `PORT` env and check `id` at `DELETE` route

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -70,6 +70,13 @@ export const updateProduct = async (req, res) => {
 export const deleteProduct = async (req, res) => {
   const { id } = req.params;
 
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({
+      success: false,
+      message: "Invalid Product ID",
+    });
+  }
+
   try {
     await Product.findByIdAndDelete(id);
     res.status(200).json({
@@ -77,9 +84,9 @@ export const deleteProduct = async (req, res) => {
       message: "Product deleted successfully",
     });
   } catch (error) {
-    res.status(404).json({
+    res.status(500).json({
       success: false,
-      message: "Product not found",
+      message: "Server error",
     });
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,9 @@ app.use(express.json());
 // Applying routes
 app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server started at http://localhost:5000");
+  console.log(`Server started at http://localhost:${PORT}`);
 });
