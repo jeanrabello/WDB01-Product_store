6337f1329d3b031a12c9fa682cf353904af8f099
Jean Rabello <jean.rabello.prog@gmail.com>
2025-03-01 16:44:03 -0300

    feat: create `Home` page and `list products`

---
+++ frontend/src/components/ProductCard.jsx
@@ -0,0 +1,84 @@
+import {
+  Box,
+  Heading,
+  HStack,
+  IconButton,
+  Image,
+  Text,
+  useColorModeValue,
+  useToast,
+} from "@chakra-ui/react";
+import { useProductStore } from "../store/product";
+import { EditIcon, DeleteIcon } from "@chakra-ui/icons";
+
+const ProductCard = ({ product }) => {
+  const textColor = useColorModeValue("gray.600", "gray.200");
+  const bg = useColorModeValue("white", "gray.800");
+
+  const { deleteProduct } = useProductStore();
+  const toast = useToast();
+
+  const handleDeleteProduct = async (id) => {
+    const { success, message } = await deleteProduct(id);
+
+    if (!success) {
+      toast({
+        title: "Error",
+        description: message,
+        status: "error",
+        isClosable: true,
+      });
+      return;
+    }
+
+    toast({
+      title: "Success",
+      description: message,
+      status: "success",
+      isClosable: true,
+    });
+  };
+  return (
+    <Box
+      shadow={"lg"}
+      rounded={"lg"}
+      overflow={"hidden"}
+      transition={"all 0.3s"}
+      _hover={{ transform: "translateY(-5px)", shadow: "xl" }}
+      bg={bg}
+    >
+      <Image
+        src={product.image}
+        alt={product.name}
+        h={48}
+        w="full"
+        objectFit={"cover"}
+      />
+
+      <Box p={4}>
+        <Heading as="h3" size={"md"} mb={2}>
+          {product.name}
+        </Heading>
+
+        <Text fontWeight={"bold"} fontSize={"xl"} color={textColor} mb={4}>
+          {product.price}
+        </Text>
+
+        <HStack spacing={2}>
+          <IconButton
+            icon={<EditIcon />}
+            onClick={() => {}}
+            colorScheme="blue"
+          />
+          <IconButton
+            icon={<DeleteIcon />}
+            onClick={() => handleDeleteProduct(product._id)}
+            colorScheme="red"
+          />
+        </HStack>
+      </Box>
+    </Box>
+  );
+};
+
+export default ProductCard;
