01415f22d5b0cc724a9dad2068a52692871d69c4
Jean Rabello <jean.rabello.prog@gmail.com>
2025-03-01 17:36:18 -0300

    build: prepare application for `deploy`

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import path from "path";
 
 // Importing routes
 import productRoutes from "./routes/product.route.js";
@@ -8,6 +9,7 @@ import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 app.use(express.json());
 
 // Applying routes
@@ -15,7 +17,14 @@ app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  connectDB();
-  console.log(`Server started at http://localhost:${PORT}`);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(PORT, async () => {
+  await connectDB();
+  console.log(`Server started`);
 });
